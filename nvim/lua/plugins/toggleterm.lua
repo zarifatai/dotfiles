a59238a23ee6384df9a21917f32de269e3f6b2f7
Zarif Atai <z.atai@protonmail.com>
2024-10-25 23:36:13 +0200

    Disabled toggleterm in insert mode

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,8 @@ return {
     config = function()
         require('toggleterm').setup {
             open_mapping = [[<leader>\]],
-            direction = 'float'
+            direction = 'float',
+            insert_mappings = false
         }
     end
 }
